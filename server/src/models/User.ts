d73de5683ec74ca7730d62364f669ea5e0558d37
Praneeth Ravuri <pravdevrav@gmail.com>
2024-07-30 18:57:13 -0700

    add: user schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,13 +1,24 @@
 import mongoose, { Schema, Document } from "mongoose"
 
 export interface IUser extends Document {
-    name: string,
-    email: string,
+    name: string;
+    userName: string;
+    email: string;
+    favoriteThemeRooms: mongoose.Types.ObjectId[];
+    favoriteStories: mongoose.Types.ObjectId[];
 }
 
 const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true }
-})
+    userName: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    favoriteThemeRooms: [{ type: mongoose.Types.ObjectId, ref: "ThemeRoom" }],
+    favoriteStories: [{ type: mongoose.Types.ObjectId, ref: "Story" }],
+
+},
+    {
+        timestamps: true
+    }
+)
 
 export default mongoose.model<IUser>('User', UserSchema)
