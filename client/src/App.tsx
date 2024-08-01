044640df29504fa88a05ff3f98d73d85bfe8a81b
Praneeth Ravuri <pravdevrav@gmail.com>
2024-07-31 22:39:23 -0700

    add: styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Home />
+      
     </div>
   );
 };
 
 export default App
+
