a906c91a39a036193593b84e933e8bb8c1b17228
Praneeth Ravuri <pravdevrav@gmail.com>
2024-08-01 19:00:37 -0700

    add: react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/layout/Sidebar';
 import Home from './pages/Home';
+import Favorites from './pages/Favorites';
+import Settings from './pages/Settings';
+import Help from './pages/Help';
 import { ThemeProvider } from './components/layout/theme-provider';
-
 
 const App: React.FC = () => {
   return (
@@ -11,11 +14,15 @@ const App: React.FC = () => {
         <div className="layout md:flex md:space-x-8">
           <Sidebar />
           <div className="content ml-16 md:ml-64 p-8">
-            <Home />
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/favorites" element={<Favorites />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/help" element={<Help />} />
+            </Routes>
           </div>
         </div>
       </ThemeProvider>
-
     </div>
   );
 };
--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { SidebarBody, SidebarItem } from '../ui/SidebarComponent';
 import { House, Heart, LogOut, Settings, LifeBuoy } from 'lucide-react';
 
 const Sidebar = () => {
+    const location = useLocation();
+
     return (
         <SidebarBody>
-            <SidebarItem icon={<House size={20} />} text="Home" active={true} />
-            <SidebarItem icon={<Heart size={20} />} text="Favorites" active={false} />
+            <Link to="/home">
+                <SidebarItem icon={<House size={20} />} text="Home" active={location.pathname === '/home'} />
+            </Link>
+            <Link to="/favorites">
+                <SidebarItem icon={<Heart size={20} />} text="Favorites" active={location.pathname === '/favorites'} />
+            </Link>
             <SidebarItem icon={<LogOut size={20} />} text="Logout" active={false} />
             <hr className='my-3 border-primary/10' />
-            <SidebarItem icon={<Settings size={20} />} text="Settings" active={false} />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" active={false} />
+            <Link to="/settings">
+                <SidebarItem icon={<Settings size={20} />} text="Settings" active={location.pathname === '/settings'} />
+            </Link>
+            <Link to="/help">
+                <SidebarItem icon={<LifeBuoy size={20} />} text="Help" active={location.pathname === '/help'} />
+            </Link>
         </SidebarBody>
     )
 }
