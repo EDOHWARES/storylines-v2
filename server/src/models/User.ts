53225b2c1023e758d72496b85f0d3d43c83ecfc6
Praneeth Ravuri <pravdevrav@gmail.com>
2024-08-13 13:29:53 -0700

    Update User.ts

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,21 +4,31 @@ export interface IUser extends Document {
     name: string;
     userName: string;
     email: string;
-    favoriteThemeRooms: mongoose.Types.ObjectId[];
-    favoriteStories: mongoose.Types.ObjectId[];
+    socialLinks: {
+        facebook?: string,
+        instagram?: string,
+        youtube?: string,
+        personalWebsite?: string,
+        twitter?: string,
+        github?: string
+    };
 }
 
 const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
     userName: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    favoriteThemeRooms: [{ type: mongoose.Types.ObjectId, ref: "ThemeRoom" }],
-    favoriteStories: [{ type: mongoose.Types.ObjectId, ref: "Story" }],
-
-},
-    {
-        timestamps: true
+    socialLinks: {
+        facebook: { type: String },
+        instagram: { type: String },
+        youtube: { type: String },
+        personalWebsite: { type: String },
+        twitter: { type: String },
+        github: { type: String }
     }
-)
+},
+{
+    timestamps: true
+})
 
 export default mongoose.model<IUser>('User', UserSchema)
