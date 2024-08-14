14fc6a80aa551ecb3fe9d02feab3afe31f0f88a4
Praneeth Ravuri <pravdevrav@gmail.com>
2024-08-13 17:09:33 -0700

    updated

--- server/src/models/Story.ts
+++ server/src/models/Story.ts
@@ -1,27 +1,26 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IStory extends Document {
-  customId?: string;
   title: string;
-  type: 'root' | 'child';
   content: string;
+  authorId: mongoose.Types.ObjectId;
   themeRoomId: mongoose.Types.ObjectId;
+  type: 'root' | 'child';
   prev: mongoose.Types.ObjectId[];
   next: mongoose.Types.ObjectId[];
 }
 
-const StorySchema: Schema = new Schema(
-  {
-    customId: { type: String, unique: true, sparse: true },
-    title: { type: String, required: true },
-    type: { type: String, enum: ['root', 'child'], required: true },
-    content: { type: String, required: true },
-    themeRoomId: { type: mongoose.Types.ObjectId, ref: 'ThemeRoom', required: true },
-    prev: [{ type: mongoose.Types.ObjectId, ref: 'Story' }],
-    next: [{ type: mongoose.Types.ObjectId, ref: 'Story' }]
-  },
-  { timestamps: true }
-);
+const StorySchema: Schema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  authorId: { type: mongoose.Types.ObjectId, ref: 'User', required: true, index: true },
+  themeRoomId: { type: mongoose.Types.ObjectId, ref: 'ThemeRoom', required: true, index: true },
+  type: { type: String, enum: ['root', 'child'], required: true },
+  prev: [{ type: mongoose.Types.ObjectId, ref: 'Story' }],
+  next: [{ type: mongoose.Types.ObjectId, ref: 'Story' }]
+}, {
+  timestamps: true
+});
 
 const Story = mongoose.model<IStory>('Story', StorySchema);
 
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -5,30 +5,29 @@ export interface IUser extends Document {
     userName: string;
     email: string;
     socialLinks: {
-        facebook?: string,
-        instagram?: string,
-        youtube?: string,
-        personalWebsite?: string,
-        twitter?: string,
-        github?: string
+      facebook?: string;
+      instagram?: string;
+      youtube?: string;
+      personalWebsite?: string;
+      twitter?: string;
+      github?: string;
     };
-}
-
-const UserSchema: Schema = new Schema({
+  }
+  
+  const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
     userName: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, index: true },
     socialLinks: {
-        facebook: { type: String },
-        instagram: { type: String },
-        youtube: { type: String },
-        personalWebsite: { type: String },
-        twitter: { type: String },
-        github: { type: String }
+      facebook: { type: String },
+      instagram: { type: String },
+      youtube: { type: String },
+      personalWebsite: { type: String },
+      twitter: { type: String },
+      github: { type: String }
     }
-},
-{
+  }, {
     timestamps: true
-})
+  });
 
 export default mongoose.model<IUser>('User', UserSchema)
